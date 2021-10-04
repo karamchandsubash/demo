9c85936428d3efa43c348e4d9c1080359436a56a
KARAMCHAND SUBASH K <karamchandsubash@gmail.com>
2021-10-04 12:36:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+const PORT = process.env.PORT || 3000;
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile('./index.html',null,function(error,data){
+        if(error){
+            res.writeHead(404);
+            res.write('File not Found!')
+        }else{
+            res.write(data);
+
+        }
+        res.end();
+
+    })
+}).listen(3000);
